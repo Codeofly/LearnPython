afa210596f9dfb33eb550ca59a6c588509bcd116
liyan_gitee <931665012@qq.com>
2020-06-29 15:42:14 +0800

    day06

---
+++ day06/04 编码2.py
@@ -0,0 +1,26 @@
+# s = 'fdsagsadfsad方式打开家乐福；按时fdsafdsa'
+#ip地址 端口等等
+# s1 = input('你好')
+# s1
+# s = 'laonanhai'
+# s1 = b'laonanhai'
+# print(type(s))
+# print(type(s1))
+
+
+# s = 'alex'  # str
+# s1 = s.encode('utf-8')  # bytes
+# encode 编码 ：str --- > bytes
+# s = 'hello girl'
+# s1 = s.encode('utf-8')
+# print(s1)
+
+# s = 'hello girl'
+# s1 = s.encode('gbk')
+# print(s1)
+# s = '中国'
+# s1 = s.encode('utf-8')
+# print(s1)
+s = '中国'
+s1 = s.encode('utf-8')
+print(s1)
