b2341f619623aef3f51c5365f21743cb80a14bfc
liyan_gitee <931665012@qq.com>
2020-06-29 17:23:41 +0800

    day05  -   dict

--- day05/04 字典的嵌套.py
+++ day05/04 字典的嵌套.py
@@ -1,24 +1,31 @@
-# dic = {"name_list":['张三','lisi','隔壁王叔叔'],
-#        'dic2':{'name':'太白','age':12},
-#        }
-# #1 ,给列表追加一个元素：'旺旺'
-# # l1 = dic['name_list']
-# # l1.append('旺旺')
-# dic['name_list'].append('旺旺')
-# print(dic)
-# #,2,给列表lisi全部大写
-# # print(dic['name_list'][1].upper())
-# dic['name_list'][1] = dic['name_list'][1].upper()
-# print(dic)
-# #3，给dic2 对应的字典添加一个键值对：hobby：girl.
-# dic['dic2']['hobby'] = 'girl'
-# print(dic)
+dic = {"name_list": ['张三', 'lisi', '隔壁王叔叔'],
+       'dic2': {'name': '太白', 'age': 12},
+       }
+
+'''
+#1 ,给列表追加一个元素：'旺旺'
+# l1 = dic['name_list']
+# l1.append('旺旺')
+dic['name_list'].append('旺旺')
+print(dic)
+
+
+#,2,给列表lisi全部大写
+# print(dic['name_list'][1].upper())
+dic['name_list'][1] = dic['name_list'][1].upper()
+print(dic)
+
+
+#3，给dic2 对应的字典添加一个键值对：hobby：girl.
+dic['dic2']['hobby'] = 'girl'
+print(dic)
+'''
 
 dic = {
-    'name':'汪峰',
-    'age':48,
-    'wife':[{'name':'国际章','age':38}],
-    'children':{'girl_first':'小苹果','girl_second':'小怡','girl_three':'顶顶'}
+    'name': '汪峰',
+    'age': 48,
+    'wife': [{'name': '国际章', 'age': 38}],
+    'children': {'girl_first': '小苹果', 'girl_second': '小怡', 'girl_three': '顶顶'}
 }
 
 name = dic['name']
@@ -34,7 +41,6 @@ print(di)
 # 当然上面是分布获取的，我们还可以合并去写：
 di2 = dic['wife'][0]
 print(di2)
-
 
 print(l1[0]['name'])
 
