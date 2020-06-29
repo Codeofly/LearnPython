b4d9b8f96367ec481eb992623457e8041368254b
liyan_gitee <931665012@qq.com>
2020-06-29 17:43:13 +0800

    day06  -   编码，解码

--- day06/04 编码2.py
+++ day06/04 编码2.py
@@ -1,5 +1,5 @@
 # s = 'fdsagsadfsad方式打开家乐福；按时fdsafdsa'
-#ip地址 端口等等
+# ip地址 端口等等
 # s1 = input('你好')
 # s1
 # s = 'laonanhai'
@@ -23,4 +23,13 @@
 # print(s1)
 s = '中国'
 s1 = s.encode('utf-8')
+
 print(s1)
+
+#解码 一
+s2 = str(s.encode('utf-8'), encoding='utf-8')
+print(s2)
+
+#解码 二
+s3 = s1.decode('utf-8')
+print(s3)
