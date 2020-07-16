fc0fee24e5893ef4283f55dc4596122d7f82368b
liyan_gitee <931665012@qq.com>
2020-07-16 18:34:28 +0800

    update github and gitee

---
+++ test.py
@@ -0,0 +1,26 @@
+class B:
+    # def __str__(self):
+    #     return " b str"
+
+    def __repr__(self):
+        return " b repr"
+
+
+class A(B):
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        # return self.name
+
+    # def __str__(self):
+    #     return self.age + "," + self.name
+
+    # def __repr__(self):
+    #     return self.age + "," + self.name
+
+
+a = A("a", "1234")
+print(a.name)
+
+print(str(a))
